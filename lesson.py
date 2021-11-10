3a13c9b755f2c77c70007c4ed3d355569605aed9
StanislavUIT <klibashev.s@beldor.centr.by>
2021-11-10 18:55:39 +0300

    сделал брови

--- lesson.py
+++ lesson.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((400, 400))
 
 N = 10
 color = (255, 255, 255)
+rect(screen, "white", [0,0,400,400])
 class my_face:
     class skin:
         circle(screen,color="yellow", radius=150, center=[200,200])
@@ -17,6 +18,9 @@ class my_face:
         circle(screen,color="black", radius=15, center=[140,150])
         circle(screen,color="red", radius=25, center=[260,150])
         circle(screen,color="black", radius=10, center=[260,150])
+    class brows:
+        line(screen, "black", [90, 60], [190, 140], 10)
+        line(screen, "black", [200, 140], [300, 90], 10)
 
 
 
